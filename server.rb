9dcf098deb85e4f471652055563947af857a8430
Danny Chang <dannychachang@gmail.com>
2018-01-04 15:40:10 -0500

    Add a tags for links and favorites

--- server.rb
+++ server.rb
@@ -9,5 +9,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['headspins', 'perfect pitch', 'I can run faster than a car', 'See Es Es', 'Backflip over a stack of 5 chairs']
+  @interests = ['chilling in general', 'kitty martial arts', 'art for winners', 'music for babies', 'films for the courageous', 'coffee brewed from tibetan monks']
   erb:about_me
+end
+
+get '/favorites' do
+  @fave_links = ["google.com", "hover.com", "amazon.ca", "reddit.com"]
+  erb:favorites
 end
